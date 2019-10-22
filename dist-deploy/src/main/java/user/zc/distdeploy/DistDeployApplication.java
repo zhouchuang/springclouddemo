95141fa09d56cd8a082d5d67bd836d3e5a8e1505
zhouchuang <zhouchuang1217@163.com>
2019-10-23 00:15:36 +0800

    lcn

---
+++ dist-deploy/src/main/java/user/zc/distdeploy/DistDeployApplication.java
@@ -0,0 +1,13 @@
+package user.zc.distdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistDeployApplication.class, args);
+    }
+
+}
